f77df355e5ba7ffb481fe8598f45b727994e7123
Ejoflo <ejoflo@icloud.com>
2019-08-17 12:42:59 -1000

    add bells and whistles

--- calculator.js
+++ calculator.js
@@ -1,50 +1,47 @@
 let screen = document.querySelector('#scrn');
 let result = null;
+let resultString = '';
 let operatorSymbol = null;
 let equalPressed = false;
 
-function getUserInput () {
-  let digitButton = document.querySelectorAll('button');
+function getUserInput () {   // listens for user input
+  let calcButton = document.querySelectorAll('button');
 
-  digitButton.forEach(function(button) {
+  calcButton.forEach(function(button) {
     button.addEventListener('click', () => {
 
-      if ((equalPressed == true) && 
-        (button.textContent !== '+') &&
-        (button.textContent !== '-') &&
-        (button.textContent !== '×') &&
-        (button.textContent !== '÷')) 
-      {   // if user presses equals, clear values
-          screen.textContent = '';
-          result = null;
-          operatorSymbol = null;
-          equalPressed = false;
-          screen.setAttribute('style', 'color: white');
-      } 
+      if (equalPressed == true && !isNaN(button.textContent) || button.textContent == 'C') {   // clear the screen
+        clearScreen(button.textContent);
       
-      if (!isNaN(button.textContent) || button.textContent == '.') {   // display digits on screen
+      } else if (!isNaN(button.textContent) || button.textContent == '.') {   // display digits on screen
         showOnScreen(button.textContent);  
       
-      } else if ((button.textContent == 'C') || (button.textContent == '⌫') || (button.textContent == '=')) {   // perform a utility function
-        updateDisplay(button.textContent);
-      
+      } else if (button.textContent == '⌫' || button.textContent == '=') {   // perform a utility function
+        runUtility(button.textContent);
+
       } else {   // perform an operation
         operate(button.textContent);
-        operatorSymbol = button.textContent;        
+        operatorSymbol = button.textContent;      
       }
     });  
   });
 }
 
-function showOnScreen (digit) {   
+function showOnScreen(digit) {   
   screen.setAttribute('style', 'color: white');
 
   if ((digit == 0) && (screen.textContent == 0)) {   // prevents users from inputting zeroes
     screen.textContent = 0;
   
-  } else if (screen.textContent.length > 10) {   // warn user if digits exceed 10
-    alert ('Cannot exceed 10 digits. Please try again.');
+  } else if (screen.textContent.length > 10) {   // prevents user from inputting more than 10 digits
+
+  // } else if (screen.textContent.indexOf('.') != -1) {   // prevents users from inputting more than one decimal
   
+  } else if (screen.textContent.split('.').length-1 > 1) {   // prevents users from inputting more than one decimal
+    screen.textContent = screen.textContent.slice(0, -1);
+
+console.log('after decimal: ' + screen.textContent);
+
   } else if ((screen.textContent == 0) && (digit != 0)) {   // displays the first non-zero number
     screen.textContent = '';
     screen.textContent += digit;
@@ -54,17 +51,20 @@ function showOnScreen (digit) {
   }
 }
 
-function operate(operator) {
+function operate(operator) {   // perform calculations
   switch (operator) {
       
     case '+':
       if (result == null) {
         result = +screen.textContent;
 
-      } else if ((operatorSymbol != '+') && (operatorSymbol != null)) {
+      } else if ((operatorSymbol != '+') && (operatorSymbol != null)) {   // use a different operator
         operate(operatorSymbol);
         
-      } else if (operatorSymbol != null) {
+      } else if ((operatorSymbol == '+') && (screen.textContent == '')) {   // prevents multiple operator presses
+        screen.textContent = '';
+
+      } else if (operatorSymbol == '+') {
         add(screen.textContent);
       
       } else if ((equalPressed == true) && (operatorSymbol == null)) {
@@ -78,10 +78,13 @@ function operate(operator) {
       if (result == null) {
         result = +screen.textContent;
       
-      } else if ((operatorSymbol != '-') && (operatorSymbol != null)) {
+      } else if ((operatorSymbol != '-') && (operatorSymbol != null)) {   // use a different operator
         operate(operatorSymbol);
       
-      } else if (operatorSymbol != null) {
+      } else if ((operatorSymbol == '-') && (screen.textContent == '')) {   // prevents multiple operator presses
+        screen.textContent = '';
+
+      } else if (operatorSymbol == '-') {
         subtract(screen.textContent);
       
       } else if ((equalPressed == true) && (operatorSymbol == null)) {
@@ -95,10 +98,13 @@ function operate(operator) {
       if (result == null) {
         result = +screen.textContent;
 
-      } else if ((operatorSymbol != '×') && (operatorSymbol != null)) {
+      } else if ((operatorSymbol != '×') && (operatorSymbol != null)) {   // use a different operator
         operate(operatorSymbol);
 
-      } else if (operatorSymbol != null) {
+      } else if ((operatorSymbol == '×') && (screen.textContent == '')) {   // prevents multiple operator presses
+        screen.textContent = '';
+
+      } else if (operatorSymbol == '×') {
         multiply(screen.textContent);
 
       } else if ((equalPressed == true) && (operatorSymbol == null)) {
@@ -112,10 +118,13 @@ function operate(operator) {
       if (result == null) {
         result = +screen.textContent;
 
-      } else if ((operatorSymbol != '÷') && (operatorSymbol != null)) {
+      } else if ((operatorSymbol != '÷') && (operatorSymbol != null)) {   // use a different operator
         operate(operatorSymbol);
-        
-      } else if (operatorSymbol != null) {
+
+      } else if ((operatorSymbol == '÷') && (screen.textContent == '')) {   // prevents multiple operator presses
+        screen.textContent = '';
+
+      } else if (operatorSymbol == '÷') {
         divide(screen.textContent);
       
       } else if ((equalPressed == true) && (operatorSymbol == null)) {
@@ -130,45 +139,56 @@ function operate(operator) {
     }
 }
 
-function add(number) {
-
-  return result += +number;
+function add(number) {   // add calculation
+   result += +number;
 }
 
-function subtract(number) {
-
-  return result -= +number;
-
+function subtract(number) {   // subtract calculation
+   result -= +number;
 }
 
-function multiply(number) {
-
-  return result *= number;
+function multiply(number) {   // multiply calculation
+   result *= +number;
 }
 
-function divide(number) {
-  if (number != 0) {
-    return result /= number;
+function divide(number) {   // divide calculation
+  if (number != '0') {
+    result = result / +number;
+
   } else {
-    alert("Can't divide by nothing! Please enter a different number.")
+    alert("Can't divide by nothing.");
+    clearScreen('C');
   }
 }
 
-function updateDisplay(operator) {
-  switch (operator) {
-  
+function clearScreen(utilityButton) {   // clear the screen and reset values
+  switch (utilityButton) {
+
     case 'C':
       screen.textContent = 0;
       result = null;
       operatorSymbol = null;
       equalPressed = false;
-      screen.setAttribute('style', 'color: white');
       break;
 
+    default:
+      screen.textContent = '';
+      result = null;
+      operatorSymbol = null;
+      equalPressed = false;
+      screen.textContent = utilityButton;
+      break;
+  }
+  screen.setAttribute('style', 'color: white');
+}
+
+function runUtility(utilityButton) {   // clear, backspace, and equals utilities
+  switch (utilityButton) {
+
     case '⌫':
-      if (screen.textContent != '') {
+      if ((screen.textContent != '') && (equalPressed != true)) {
         screen.textContent = screen.textContent.slice(0, -1);
-      }
+      } 
       break;
       
     case '=':
@@ -177,37 +197,65 @@ function updateDisplay(operator) {
 
     default:
       break;
-    }
+  }
 }
 
-function finalResult(opSymbol) {
+function finalResult(opSymbol) {   // perform final calculations
   screen.setAttribute('style', 'color: yellow');
   
   switch (opSymbol) {
   
     case '+':
-      screen.textContent = +result + +screen.textContent;
+      resultString = +result + +screen.textContent;
+      if ((String(resultString).length) >= 7) {   // check to see if number is over 10 digits
+        resultString = Number(resultString);
+        screen.textContent = resultString.toPrecision(7);
+      } else {
+        screen.textContent = resultString;
+      }
       result = +screen.textContent;
       operatorSymbol = null;
       equalPressed = true;
       break;
 
     case '-':
-      screen.textContent = +result - +screen.textContent;
+      resultString = +result - +screen.textContent;
+      if ((String(resultString).length) >= 7) {   // check to see if number is over 10 digits
+        resultString = Number(resultString);
+        screen.textContent = resultString.toPrecision(7);
+      } else {
+        screen.textContent = resultString;
+      }
       result = +screen.textContent;
       operatorSymbol = null;
       equalPressed = true;
       break;
 
     case '×':
-      screen.textContent = +result * +screen.textContent;
+      resultString = +result * +screen.textContent;
+      if ((String(resultString).length) >= 7) {   // check to see if number is over 10 digits
+        resultString = Number(resultString);
+        screen.textContent = resultString.toPrecision(7);
+      } else {
+        screen.textContent = resultString;
+      }
       result = +screen.textContent;
       operatorSymbol = null;
       equalPressed = true;
       break;
       
     case '÷':
-      screen.textContent = +result / +screen.textContent;
+      if (+screen.textContent == 0) {   // protect from dividing by zero
+        screen.textContent = 'NOPE';
+      } else {
+        resultString = +result / +screen.textContent;
+        if ((String(resultString).length) >= 7) {   // check to see if number will overflow display
+          resultString = Number(resultString);
+          screen.textContent = resultString.toPrecision(7);
+        } else {
+          screen.textContent = resultString;
+        }
+      }
       result = +screen.textContent;
       operatorSymbol = null;
       equalPressed = true;
@@ -215,4 +263,4 @@ function finalResult(opSymbol) {
   }
 }
 
-getUserInput();
+getUserInput();   // run the calculator 
